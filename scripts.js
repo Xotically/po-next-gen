9bde9807d49500018a56229b18f1687fe3350ace
Xotically <anonware@cyberservices.com>
2016-10-19 18:25:59 +0100

    Custom Scripts

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
 /*jshint laxbreak:true,shadow:true,undef:true,evil:true,trailing:true,proto:true,withstmt:true*/
 // You may change these variables as long as you keep the same type
 var Config = {
-    base_url: "https://raw.githubusercontent.com/Server-Scripts/po-server-goodies/master/scripts.js",
+    base_url: "https://raw.githubusercontent.com/Xotically/safari/master/scripts.js",
     dataDir: "scriptdata/",
     bot: "Bot",
     kickbot: "Kickbot",
